78b6a42aaf9885aa05029a7a629fd98e55e71743
Lms-media <50673591+Lms-media@users.noreply.github.com>
2025-03-07 18:49:22 +0300

    Update spaces

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,8 +2,8 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
-    int64_t ans = 1;
+  int64_t ans = 1;
     for(uint16_t i = 0; i < n; i++)
-        ans *= x;
-    return ans;
+      ans *= x;
+  return ans;
 }
